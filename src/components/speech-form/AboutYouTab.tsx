dc7100ee3dd563b204f5694ff93131a19049099c
simmersauce <105205997+simmersauce@users.noreply.github.com>
2025-03-02 10:06:48 +1000

    Update AboutYouTab.tsx

--- src/components/speech-form/AboutYouTab.tsx
+++ src/components/speech-form/AboutYouTab.tsx
@@ -90,7 +90,7 @@ const AboutYouTab = ({
 
       <FormField
         control={form.control}
-        name="graduatingClass"
+        name="graduationClass"
         render={({ field }) => (
           <FormItem>
             <FormLabel>Graduating Class</FormLabel>
