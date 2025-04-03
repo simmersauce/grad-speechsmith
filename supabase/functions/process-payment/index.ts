44d54a1f94c530eb85521909ea7b525b7c73bc5c
simmersauce <105205997+simmersauce@users.noreply.github.com>
2025-04-03 11:06:45 +1000

    Update index.ts

--- supabase/functions/process-payment/index.ts
+++ supabase/functions/process-payment/index.ts
@@ -3,7 +3,7 @@ import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { initSentry } from "../shared/sentry.ts";
 
-// Initialize Sentry
+// Initialize Sentry for function
 const sentry = initSentry("process-payment");
 
 const corsHeaders = {
